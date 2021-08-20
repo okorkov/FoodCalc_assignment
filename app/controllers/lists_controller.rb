12d6a4ed3207322a8b91b0917dc13e2079d1c7eb
okorkov <alex.okorkov@gmail.com>
2021-08-20 13:00:47 -0700

    added list edit and show views and controller actions

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -10,10 +10,36 @@ class ListsController < ApplicationController
 
   def new
     @list = List.new
+    @items = Item.all
   end
 
   def create
+    list = List.new(list_params)
+    list.user = current_user
+    list.save
+    redirect_to user_lists_path(current_user)
+  end
+
+  def show
+    @list = List.find_by(id: params[:id])
+  end
+
+  def edit
+    @list = List.find_by(id: params[:id])
+  end
+
+  def update
     raise params.inspect
+  end
+
+  def destroy
+    
+  end
+
+  private
+
+  def list_params
+    params.require(:list).permit(:name, :is_favorite, item_ids: [])
   end
 
 end
