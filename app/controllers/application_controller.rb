8ef6b129ca7bd2dab884e05f4557278dc2315bdc
okorkov <alex.okorkov@gmail.com>
2021-08-20 09:51:45 -0700

    added nav bar and other layout

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,10 @@ class ApplicationController < ActionController::Base
 
   include ApplicationHelper 
 
+  layout 'welcome'
+
   def welcome
+    redirect_to user_lists_path(current_user) if logged_in?
     @user = User.new
   end
 
--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -2,6 +2,8 @@ class ListsController < ApplicationController
 
   before_action :require_login
 
+  layout 'application'
+
   def index
     @lists = current_user.lists
   end
